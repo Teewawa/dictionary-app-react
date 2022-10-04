a243cb4b687f771ab38de69d0af2a7018df0bbaf
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-04 14:28:37 -0500

    Added a Dictionary component, container, & footer
    Container wrapper is to apply Bootstrap styling. Footer contains credits and open source link.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <Dictionary />
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/tiaramendiola/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tiara Mendiola
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Teewawa/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
