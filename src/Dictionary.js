78cd0dd657aa5be334d930d568489f004f7491db
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-04 14:30:13 -0500

    Created Dictionary.js component
    Added a form for dictionary's search engine, created function to handle event for user input

---
+++ src/Dictionary.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+    event.target.reset();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          autoFocus={true}
+          className="form-control"
+          placeholder="Search for a word"
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
