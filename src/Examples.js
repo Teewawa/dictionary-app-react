d896e6a2c4053defea800f0d495566d225564ae0
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-08 16:30:57 -0500

    removed console.log

--- src/Examples.js
+++ src/Examples.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Examples(props) {
-  console.log("out", props.definition);
   if (props.definition.example !== undefined) {
     return <div className="Examples">"{props.definition.example}"</div>;
   } else {
