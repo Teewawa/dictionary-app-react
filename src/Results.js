b3a87b5280786a92315a5c3753a9f3e631b7878e
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-11 15:36:19 -0500

    Import and add Phonetics component
    Map through phonetic (response) data and pass data into Phonetics component. Display in ordered list

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./Dictionary.css";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {/*Phonetics, how to separate text & audio results */}
-        <h3>{props.results.phonetic}</h3>
-        {/*Looping through each meaning */}
+
+        {/*Looping through each phonetics: find audio & text */}
+        <ol>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetics phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </ol>
+        {/*Looping through each Part of Speech Meaning(definition) */}
         {props.results.meanings.map(function (meaning, index) {
-          if (index < 4) {
+          if (index < 3) {
             return (
               <div key={index}>
                 <Meaning meaning={meaning} />
