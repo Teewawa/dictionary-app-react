04e420e13ea1f05a22da8d0af08e4fd156f6b79f
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-11 16:01:44 -0500

    Updated conditions
    to check if the object contains "audio" or "text" If audio is not defined, display text. Likewise, if text is not defined, display audio

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  if (props.phonetic.audio === "") {
+  //either audio is empty or does not exist in the object (false)
+  if (props.phonetic.audio === "" || "audio" in props.phonetic === false) {
+    //return only the text
     return <li>{props.phonetic.text}</li>;
   } else {
-    if (props.phonetic.text === "" || props.phonetic.text === "undefined") {
-      return null;
+    //either text is empty or does not exist in the object (false)
+    if (props.phonetic.text === "" || "text" in props.phonetic === false) {
+      //return only the audio
+      return (
+        <li>
+          <a
+            href={props.phonetic.audio}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Listen
+          </a>
+        </li>
+      );
+      //both audio & text is within object, return both
     } else if (props.phonetic.audio && props.phonetic.text) {
       return (
         <div className="Phonetics">
