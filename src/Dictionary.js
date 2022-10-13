583a14b3e15e55259fe8b79b2a67d60d9f75db86
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-13 15:39:01 -0500

    Display a default definition on load
    Added function to handle default word search. Added conditional rendering to prevent repeated API call for initial/default word search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,16 @@ import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.default);
   let [results, setResults] = useState(null);
+  let [ready, setReady] = useState(false);
+
+  //Display a default definition, keyword received as prop from App.js
+  function displayDefault() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function handleResponse(response) {
     console.log(response.data[0]);
@@ -31,18 +38,38 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          className="form-control"
-          placeholder="Search for a word"
-          onChange={handleKeywordChange}
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  //Conditional Rendering to prevent repeated API call on initial search
+  if (ready === false) {
+    displayDefault();
+    setReady(true);
+    return (
+      <div className="Dictionary">
+        <form onSubmit={search}>
+          <input
+            type="search"
+            autoFocus={true}
+            className="form-control"
+            placeholder="Search for a word"
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={search}>
+          <input
+            type="search"
+            autoFocus={true}
+            className="form-control"
+            placeholder="Search for a word"
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <Results results={results} />
+      </div>
+    );
+  }
 }
