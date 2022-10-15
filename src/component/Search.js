c85671f651b0686683522f438ef6ca8e79a3be1c
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-15 13:59:50 -0500

    Moved Search.js into component folder

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./css/Search.css";
+import "../css/Search.css";
 import axios from "axios";
 import Dictionary from "./Dictionary";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
