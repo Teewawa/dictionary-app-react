7961d00b2f13b68eda00ad1192fbdf5fa4ab3b23
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-14 19:55:28 -0500

    Renamed from Meaning to Definitions & update:
    updated (props) from meaning to partOfSpeechData, added className to li element for styling, & moved file into component folder

---
+++ src/component/Definitions.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../css/Definitions.css";
+import Examples from "../Examples";
+import Synonyms from "../Synonyms";
+
+export default function Definitions(props) {
+  return (
+    <div className="Definitions">
+      <h3>{props.partOfSpeechData.partOfSpeech}</h3>
+      <ol>
+        {props.partOfSpeechData.definitions.map(function (definition, index) {
+          if (index < 3) {
+            return (
+              <li
+                className="definitions-text"
+                key={index}
+              >
+                {" "}
+                {definition.definition}
+                <Examples definition={definition} />
+                <Synonyms definition={definition} />
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </ol>
+    </div>
+  );
+}
+
+/*Definition organizes received data/props:
+Part of Speech: definition(s), example(s), & synonym(s)
+*/
