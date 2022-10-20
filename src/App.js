54ffbba15871e60adc3b98e4163a3168496d9017
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-20 16:30:15 -0500

    Updated footer content
    Added section for controlling content styling

--- src/App.js
+++ src/App.js
@@ -8,26 +8,28 @@ function App() {
         <header className="App-header">
           <h1>Dictionary</h1>
         </header>
-        <Search default="hello" />
+        <Search default="words" />
         <footer className="App-footer">
-          <span>
-            This project was coded by{" "}
-            <a
-              href="https://www.linkedin.com/in/tiaramendiola/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Tiara Mendiola
-            </a>{" "}
-            and is{" "}
-            <a
-              href="https://github.com/Teewawa/react-weather"
-              target="_blank"
-              rel="noreferrer"
-            >
-              open-sourced.
-            </a>
-          </span>
+          <section>
+            <span>
+              An{" "}
+              <a
+                href="https://github.com/Teewawa/react-weather"
+                target="_blank"
+                rel="noreferrer"
+              >
+                open-sourced
+              </a>{" "}
+              project coded by{" "}
+              <a
+                href="https://www.linkedin.com/in/tiaramendiola/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Tiara Mendiola
+              </a>
+            </span>
+          </section>
         </footer>
       </div>
     </div>
