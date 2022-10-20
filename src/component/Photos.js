674ac0d86618cfbcdbf1cb71de7a357f04a9a5b4
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-20 16:32:47 -0500

    Created Photo.js component
    to display photos based off searched keyword

---
+++ src/component/Photos.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../css/Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <div className="Photos">
+        <section>
+          <div className="row">
+            {props.photos.map(function (photo, index) {
+              return (
+                <div
+                  className="col-4"
+                  key={index}
+                >
+                  <a
+                    href={photo.src.original}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={photo.src.landscape}
+                      alt={photo.alt}
+                      className="img-fluid"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
